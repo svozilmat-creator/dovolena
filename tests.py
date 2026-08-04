c4b0ca86b62838508e221cae4eab1ca7bbdcb4cf
svozilmat-creator <svozilmat@gmail.com>
2026-08-04 22:57:58 +0200

    Delete tests.py

--- tests.py
+++
@@ -1,8 +0,0 @@
-import json,pathlib
-r=pathlib.Path(__file__).parent;d=json.load(open(r/'destinations.json'))
-assert len(d)==100 and len({x['id'] for x in d})==100
-for x in d:
- assert len(x['seasonality'])==12
- assert (r/x['image']).exists()
- assert all(0<=v<=10 for v in x['scores'].values())
-print('OK',len(d),'destinací')
